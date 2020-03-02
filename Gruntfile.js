dfd70377b725f1288c2804a7500296056ba3409b
shangchunli <1503651223@qq.com>
2020-03-02 18:39:19 +0800

    csslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,14 @@
 module.exports = function (grunt) {
     grunt.initConfig({
-     htmlhint: {
-        option:{
-          htmlhint:'./.htmlhintrc'
+     csslint: {
+        options:{
+          csslintrc:'./.csslintrc'
         },
-        src:['*.html']
+        src:['*.css']
       }
     });
   
-    grunt.loadNpmTasks('grunt-htmlhint');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
 
-    grunt.registerTask('default', ['htmlhint']);
+    grunt.registerTask('default', ['csslint']);
   };
